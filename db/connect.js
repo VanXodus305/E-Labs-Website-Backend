568d8507f55d1dee39071b24a896bf177febde45
dead8309 <vaibhavmonu8309@gmail.com>
2024-10-07 21:20:37 +0530

    move to es modules

--- db/connect.js
+++ db/connect.js
@@ -1,10 +1,8 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
-const connect = () => {
+export const connect = () => {
   mongoose
     .connect('mongodb://localhost:27017/elabs_backend')
     .then(() => console.log('connected to db'))
     .catch(() => console.log("can't connect to db"));
 };
-
-module.exports = connect;
