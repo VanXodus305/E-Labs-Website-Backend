6302b65969fcbc06dc89a93970d5ccfd83e8c94b
VanXodus305 <soumyadeep.30505@gmail.com>
2025-02-04 23:08:53 +0530

    Implement AddMember page and integrate Tailwind CSS into the project

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router";
+import "./index.css";
+import App from "./App.jsx";
+import AddMember from "./pages/AddMember.jsx";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <Routes >
+      <Route path="*" element={<h1>Not Found</h1>}/>
+      <Route path="/" element={<App />} />
+      <Route path="addmember" element={<AddMember></AddMember>}></Route>
+    </Routes>
+  </BrowserRouter>
+);
