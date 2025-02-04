0ba53af92d09e7046156b77f5a1795140c60d491
VanXodus305 <soumyadeep.30505@gmail.com>
2025-02-05 01:50:06 +0530

    Downgraded to Tailwind v3 and setup Hero UI

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className="text-textColor1">App</div>
   )
 }
 
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import "./index.css";
+import { HeroUIProvider } from "@heroui/react";
 import App from "./App.jsx";
 import AddMember from "./pages/AddMember.jsx";
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <Routes >
-      <Route path="*" element={<h1>Not Found</h1>}/>
-      <Route path="/" element={<App />} />
-      <Route path="addmember" element={<AddMember></AddMember>}></Route>
-    </Routes>
-  </BrowserRouter>
+  <HeroUIProvider>
+    <BrowserRouter>
+      <Routes>
+        <Route path="*" element={<h1 className="font-bold text-5xl text-textColor1">Not Found</h1>} />
+        <Route path="/" element={<App />} />
+        <Route path="addmember" element={<AddMember></AddMember>}></Route>
+      </Routes>
+    </BrowserRouter>
+  </HeroUIProvider>
 );
