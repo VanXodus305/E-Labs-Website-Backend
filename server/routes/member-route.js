0d1a3216c1003c188e23bee69bab9d290de4a5bc
JagTheFriend <JagTheFriend12@gmail.com>
2025-02-07 21:15:33 +0530

    Use multer middleware to parse form data
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- server/routes/member-route.js
+++ server/routes/member-route.js
@@ -1,9 +1,10 @@
 // routes/member.js
 import express from "express";
 import { addMember } from "../controllers/member.js";
+import { upload } from "../middlewares/multer.middleware.js";
 
 const router = express.Router();
 
-router.post("/add-member", addMember);
+router.post("/add-member", upload.single("image"), addMember);
 
 export default router;
