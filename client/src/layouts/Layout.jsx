d5fedab87f0604ed3288344ce837ce92602c6182
JagTheFriend <JagTheFriend12@gmail.com>
2025-03-08 09:59:13 +0530

    refactor: Clean up imports and component usage in Layout and main files
    Signed-off-by: JagTheFriend <JagTheFriend12@gmail.com>

--- client/src/layouts/Layout.jsx
+++ client/src/layouts/Layout.jsx
@@ -1,6 +1,5 @@
-import React from "react";
-import { Outlet } from "react-router";
 import { HeroUIProvider } from "@heroui/react";
+import { Outlet } from "react-router";
 
 const Layout = () => {
   return (
