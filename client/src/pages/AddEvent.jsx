b7b06303a30df8bf94263fe782aa66b7009fecc7
VanXodus305 <soumyadeep.30505@gmail.com>
2025-05-09 20:27:52 +0530

    refactor: Enhance AddEvent component with improved form handling and loading states

--- client/src/pages/AddEvent.jsx
+++ client/src/pages/AddEvent.jsx
@@ -2,21 +2,23 @@ import React from "react";
 import {
   Input,
   Image,
-  Autocomplete,
-  AutocompleteItem,
-  TimeInput,
+  // Autocomplete,
+  // AutocompleteItem,
   Form,
   Button,
   Textarea,
-  DateInput,
   DatePicker,
+  Spinner,
+  addToast,
 } from "@heroui/react";
-import { FaCamera, FaRegClock } from "react-icons/fa";
+import { FaCamera } from "react-icons/fa";
 import { FaLocationDot } from "react-icons/fa6";
 
 const AddEvent = () => {
   const [submitted, setSubmitted] = React.useState(null);
   const [previewUrl, setPreviewUrl] = React.useState(null);
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [error, setError] = React.useState(null);
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -33,16 +35,52 @@ const AddEvent = () => {
   }, [previewUrl]);
 
   React.useEffect(() => {
-    if (submitted) {
-      console.log(submitted);
-      // Call backend API to submit data
-    }
+    if (!submitted) return;
+
+    const sendRequest = async () => {
+      setIsLoading(true);
+
+      const data = await fetch(import.meta.env.VITE_ADD_EVENT_URI, {
+        method: "POST",
+        body: submitted,
+      });
+
+      if (data.status !== 200) {
+        addToast({
+          title: "Failed to submit. Please try again.",
+          color: "danger",
+          className: "dark",
+          timeout: 6000,
+          classNames: {
+            title: "font-varela text-left",
+          },
+          radius: "lg",
+        });
+        setSubmitted(null);
+      } else {
+        document.querySelector("form").reset();
+        setPreviewUrl(null);
+        addToast({
+          title: "Submitted Successfully!",
+          className: "dark",
+          classNames: {
+            title: "font-varela text-left",
+          },
+          radius: "lg",
+          color: "success",
+          timeout: 6000,
+        });
+      }
+      setIsLoading(false);
+    };
+    sendRequest();
   }, [submitted]);
 
   const onSubmit = (e) => {
     e.preventDefault();
-    const data = Object.fromEntries(new FormData(e.currentTarget));
+    const data = new FormData(e.currentTarget);
     setSubmitted(data);
+
   };
 
   return (
@@ -66,13 +104,17 @@ const AddEvent = () => {
             <div className="w-full flex flex-row flex-wrap-reverse items-center justify-between gap-10">
               <div className="flex w-full md:w-[50%] flex-col gap-6 items-center justify-center">
                 <Input
-                  label="Name"
+                  label="Event Name"
                   variant="bordered"
                   isClearable
                   name="name"
                   color="warning"
                   isRequired
-                  classNames={{ label: "text-md" }}
+                  classNames={{
+                    label: "text-md",
+                    errorMessage: "text-left text-[13px]",
+                  }}
+                  className="w-full"
                 ></Input>
                 <Textarea
                   label="Description"
@@ -82,9 +124,12 @@ const AddEvent = () => {
                   name="desc"
                   color="warning"
                   minRows={5}
-                  classNames={{ label: "text-md" }}
+                  classNames={{
+                    label: "text-md",
+                    errorMessage: "text-left text-[13px]",
+                  }}
                 ></Textarea>
-                <Autocomplete
+                {/* <Autocomplete
                   label="Organizer/Instructor"
                   variant="bordered"
                   color="warning"
@@ -98,7 +143,7 @@ const AddEvent = () => {
                     },
                   }}
                 >
-                  {/* {domains
+                  {domains
                     .sort((d1, d2) => d1.label.localeCompare(d2.label))
                     .map((item) => (
                       <AutocompleteItem
@@ -110,8 +155,21 @@ const AddEvent = () => {
                       >
                         {item.label}
                       </AutocompleteItem>
-                    ))} */}
-                </Autocomplete>
+                    ))} 
+                </Autocomplete> */}
+                <Input
+                  label="Organizer/Instructor"
+                  variant="bordered"
+                  isClearable
+                  name="organizer"
+                  color="warning"
+                  isRequired
+                  classNames={{
+                    label: "text-md",
+                    errorMessage: "text-left text-[13px]",
+                  }}
+                  className="w-full"
+                ></Input>
                 <Input
                   label="Location"
                   variant="bordered"
@@ -119,7 +177,10 @@ const AddEvent = () => {
                   isClearable
                   name="location"
                   color="warning"
-                  classNames={{ label: "text-md" }}
+                  classNames={{
+                    label: "text-md",
+                    errorMessage: "text-left text-[13px]",
+                  }}
                   startContent={
                     <FaLocationDot className="text-lg text-textColor1" />
                   }
@@ -128,7 +189,7 @@ const AddEvent = () => {
 
               <div className="flex w-full md:w-[40%] flex-col gap-6 items-center justify-center">
                 <Image
-                  className="object-cover flex min-w-[70px] h-[180px] shadow-lg shadow-textColor1 mb-6 "
+                  className="object-cover flex min-w-[70px] h-[184px] shadow-lg shadow-textColor1 mb-6 "
                   src={
                     previewUrl ||
                     "https://t3.ftcdn.net/jpg/04/60/01/36/360_F_460013622_6xF8uN6ubMvLx0tAJECBHfKPoNOR5cRa.jpg"
@@ -145,7 +206,11 @@ const AddEvent = () => {
                   labelPlacement="outside"
                   isClearable
                   color="warning"
-                  classNames={{ label: "text-md" }}
+                  classNames={{
+                    label: "text-md",
+                    description: "text-left text-[13px]",
+                    errorMessage: "text-left text-[13px]",
+                  }}
                   startContent={
                     <FaCamera className="text-lg text-textColor1" />
                   }
@@ -161,20 +226,28 @@ const AddEvent = () => {
                   isRequired
                   showMonthAndYearPickers
                   granularity="minute"
-                  classNames={{ popoverContent: "dark" }}
+                  classNames={{
+                    popoverContent: "dark",
+                    label: "text-left text-sm",
+                    errorMessage: "text-left text-[13px]",
+                  }}
                   dateInputClassNames={{ label: "text-md text-left" }}
                 ></DatePicker>
               </div>
             </div>
 
-            <div className="flex w-full items-center justify-center gap-10">
+            <div className="flex w-full items-center justify-center flex-wrap flex-row gap-8">
               <Button
                 className="w-full max-w-[250px] hover:scale-105 transition-all ease-in-out duration-200 font-bold text-xl"
                 type="submit"
                 variant="shadow"
+                isDisabled={isLoading}
                 color="warning"
                 radius="lg"
                 size="lg"
+                startContent={
+                  isLoading && <Spinner color="default" variant="gradient" />
+                }
               >
                 Submit
               </Button>
@@ -183,17 +256,13 @@ const AddEvent = () => {
                 type="reset"
                 variant="faded"
                 radius="lg"
+                isDisabled={isLoading}
                 color="warning"
                 size="lg"
               >
                 Reset
               </Button>
             </div>
-            {submitted && (
-              <div className="w-full text-center text-lg font-semibold text-textColor1 -mt-10">
-                Submitted Successfully!
-              </div>
-            )}
           </Form>
         </div>
       </div>
